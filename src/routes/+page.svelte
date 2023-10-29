f52f6dacb338b5685c558e78b3f133cdae6b68e7
bws428 <briandwendt@gmail.com>
2023-10-29 08:19:31 -0400

    Add some test marketing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,8 +36,8 @@
 			<div class="hidden lg:flex lg:gap-x-12">
 				<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900">Product</a>
 				<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
-				<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-				<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+				<a href="#pricing" class="text-sm font-semibold leading-6 text-gray-900">Pricing</a>
+				<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900">About Us</a>
 			</div>
 			<div class="hidden lg:flex lg:flex-1 lg:justify-end">
 				<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900"
@@ -122,7 +122,7 @@
 				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 			/>
 		</div>
-		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-40">
 			<div class="hidden sm:mb-8 sm:flex sm:justify-center">
 				<div
 					class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
@@ -147,11 +147,11 @@
 					<a
 						href="/auth/login"
 						class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-						>Get started for free</a
+						>Get started for free <span aria-hidden="true">→</span></a
 					>
 					<a href="/auth/login" class="text-sm font-semibold leading-6 text-gray-900"
-						>Learn more <span aria-hidden="true">→</span></a
-					>
+						>Learn more
+					</a>
 				</div>
 			</div>
 		</div>
@@ -219,6 +219,121 @@
 					width="1824"
 					height="1080"
 				/>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- PRICING -->
+<div class="bg-white py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div class="mx-auto max-w-2xl sm:text-center">
+			<h2 id="pricing" class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+				Simple no-tricks pricing
+			</h2>
+			<p class="mt-6 text-lg leading-8 text-gray-600">
+				Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut
+				non voluptas in. Explicabo id ut laborum.
+			</p>
+		</div>
+		<div
+			class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none"
+		>
+			<div class="p-8 sm:p-10 lg:flex-auto">
+				<h3 class="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
+				<p class="mt-6 text-base leading-7 text-gray-600">
+					Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis
+					blanditiis repellendus etur quidem assumenda.
+				</p>
+				<div class="mt-10 flex items-center gap-x-4">
+					<h4 class="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
+					<div class="h-px flex-auto bg-gray-100" />
+				</div>
+				<ul
+					role="list"
+					class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+				>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-indigo-600"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Private forum access
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-indigo-600"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Member resources
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-indigo-600"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Entry to annual conference
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-indigo-600"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Official member t-shirt
+					</li>
+				</ul>
+			</div>
+			<div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+				<div
+					class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16"
+				>
+					<div class="mx-auto max-w-xs px-8">
+						<p class="text-base font-semibold text-gray-600">Pay once, own it forever</p>
+						<p class="mt-6 flex items-baseline justify-center gap-x-2">
+							<span class="text-5xl font-bold tracking-tight text-gray-900">$349</span>
+							<span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+						</p>
+						<a
+							href="#"
+							class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>Get access</a
+						>
+						<p class="mt-6 text-xs leading-5 text-gray-600">
+							Invoices and receipts available for easy company reimbursement
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -350,6 +465,104 @@
 					</dd>
 				</div>
 			</dl>
+		</div>
+	</div>
+</div>
+
+<!-- LAST -->
+
+<div class="overflow-hidden bg-white py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div
+			class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2"
+		>
+			<div class="lg:pr-8 lg:pt-4">
+				<div class="lg:max-w-lg">
+					<h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+					<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+						A better workflow
+					</p>
+					<p class="mt-6 text-lg leading-8 text-gray-600">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
+						suscipit eaque, iste dolor cupiditate blanditiis ratione.
+					</p>
+					<dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+						<div class="relative pl-9">
+							<dt class="inline font-semibold text-gray-900">
+								<svg
+									class="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+									viewBox="0 0 20 20"
+									fill="currentColor"
+									aria-hidden="true"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+								Push to deploy.
+							</dt>
+							<dd class="inline">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
+								perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+							</dd>
+						</div>
+						<div class="relative pl-9">
+							<dt class="inline font-semibold text-gray-900">
+								<svg
+									class="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+									viewBox="0 0 20 20"
+									fill="currentColor"
+									aria-hidden="true"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+								SSL certificates.
+							</dt>
+							<dd class="inline">
+								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+								commodo.
+							</dd>
+						</div>
+						<div class="relative pl-9">
+							<dt class="inline font-semibold text-gray-900">
+								<svg
+									class="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+									viewBox="0 0 20 20"
+									fill="currentColor"
+									aria-hidden="true"
+								>
+									<path
+										d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"
+									/>
+									<path
+										fill-rule="evenodd"
+										d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+								Database backups.
+							</dt>
+							<dd class="inline">
+								Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi
+								lobortis.
+							</dd>
+						</div>
+					</dl>
+				</div>
+			</div>
+			<img
+				src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+				alt="Product screenshot"
+				class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+				width="2432"
+				height="1442"
+			/>
 		</div>
 	</div>
 </div>
